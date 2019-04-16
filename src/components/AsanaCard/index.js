0123305ca34e8394e6e16160aed917c5d62abca0
Ize Imai <izeimai@gmail.com>
2019-04-15 21:35:22 -0400

    Added buttons to the cards and a dropdown to sort by category

--- src/components/AsanaCard/index.js
+++ src/components/AsanaCard/index.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import './style.css';
-import { Card, CardTitle, Row, Col } from 'react-materialize';
+import { Button, Card, CardTitle, Divider, Dropdown, Row, Col } from 'react-materialize';
 import asanaJson from '../../asana.json';
 
 
 class AsanaCard extends Component {
 
   state = {
+    categories: ["Backbends", "Balancing", "Hip", "Inversion", "Peak", "Seated", "Shoulder", "Standing", "Supine", "Twists"],
+    selected: []
   };
 
+  _renderCategory(category, i) {
+    return (
+      <a href={category} key={i} className="purple-text">
+        {category}
+      </a>
+    )
+  }
+
   _renderPose(asana, i) {
-    const { sanskrit_name, english_name, img_url, targetArea, cues } = asana;
+    const { sanskrit_name, english_name, img_url, targetArea, translation, cues } = asana;
     return (
       <Col l={3} m={6} s={12}>
+
         <Card key={i} header={<CardTitle image={img_url} />}
           title={english_name}
-          reveal={<div><p>{cues}</p><p>Great for targeting: {targetArea}</p></div>}>
+          reveal={<div><p>{cues}</p><p>Great for targeting: {targetArea}</p><p>Translation: {translation}</p></div>}>
+          <Button
+            floating
+            large
+            className="purple"
+            waves="light"
+            icon="add"
+            onClick=""
+          />
           <span>{sanskrit_name}</span>
         </Card>
       </Col>
@@ -24,9 +43,19 @@ class AsanaCard extends Component {
 
   render() {
     return (
-      <Row>
+      <div>
+        <Row>
+          <Dropdown trigger={<Button className="purple">Sort by Category</Button>}>
+            {this.state.categories.map(this._renderCategory)}
+            <Divider />
+          </Dropdown>
+        </Row>
+        <br></br>
+        <br></br>
+        <Row>
           {asanaJson.map(this._renderPose)}
-      </Row>
+        </Row>
+      </div>
     );
   }
 }
